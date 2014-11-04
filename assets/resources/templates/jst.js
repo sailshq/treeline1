cafafa9a63d8af2ce1e3b84a3af0cf89b67668ee
Cody Stoltman <particlebanana@gmail.com>
2014-11-03 18:30:55 -0600

    make pretty

---
+++ assets/resources/templates/jst.js
@@ -0,0 +1,11 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["assets/templates/spinners/button-spinner.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="button-spinner">\n  <div class="circle circle_1"></div>\n  <div class="circle circle_2"></div>\n  <div class="circle circle_3"></div>\n  <div class="circle circle_4"></div>\n  <div class="circle circle_5"></div>\n  <div class="circle circle_6"></div>\n  <div class="circle circle_7"></div>\n  <div class="circle circle_8"></div>\n</div>\n';
+
+}
+return __p
+};
