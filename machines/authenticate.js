7c2b28aa69904664a97bb2923d2f29e4d9c2d459
Mike McNeil <mike@balderdash.co>
2015-02-15 15:48:03 -0800

    Set up login and list-apps methods

--- machines/authenticate.js
+++ machines/authenticate.js
@@ -20,6 +20,11 @@ module.exports = {
     password: {
       description: 'The password for the Treeline account with this username',
       required: true
+    },
+
+    baseUrl: {
+      description: 'The base URL for the Treeline API (useful if you\'re in a country that can\'t use SSL, etc.)',
+      example: 'http://api.treeline.io'
     }
 
   },
@@ -34,6 +39,14 @@ module.exports = {
       description: 'Unexpected error occurred'
     },
 
+    forbidden: {
+      description: 'Provided secret is invalid or corrupted.  Please reauthenticate.'
+    },
+
+    requestFailed: {
+      description: 'Could not communicate with Treeline.io -- are you connected to the internet?'
+    },
+
     success: {
       description: 'Treeline secret key fetched successfully.',
       example: '29f559ae-3bec-4d0a-8458-1f4e32a72407'
@@ -46,7 +59,34 @@ module.exports = {
 
     var Http = require('machinepack-http');
 
-    return exits.success();
+
+    // Send an HTTP request and receive the response.
+    Http.sendHttpRequest({
+      method: 'put',
+      baseUrl: inputs.baseUrl||'http://api.treeline.io',
+      url: '/cli/login',
+      params: {},
+      headers: {}
+    }).exec({
+      // An unexpected error occurred.
+      error: function(err) {
+        return exits.error(err);
+      },
+      // 403 status code returned from server
+      forbidden: function(result) {
+        return exits.forbidden(result.body);
+      },
+      // Unexpected connection error: could not send or receive HTTP request.
+      requestFailed: function() {
+        return exits.requestFailed();
+      },
+      // OK.
+      success: function(result) {
+        console.log(result.body);
+        return exits.success(result.body);
+      },
+    });
+
   }
 
 };
--- machines/list-apps.js
+++ machines/list-apps.js
@@ -14,8 +14,14 @@ module.exports = {
 
     secret: {
       description: 'The Treeline secret key of the account whose apps will be listed.',
-      example: '29f559ae-3bec-4d0a-8458-1f4e32a72407'
+      example: '29f559ae-3bec-4d0a-8458-1f4e32a72407',
+      required: true
     },
+
+    baseUrl: {
+      description: 'The base URL for the Treeline API (useful if you\'re in a country that can\'t use SSL, etc.)',
+      example: 'http://api.treeline.io'
+    }
 
   },
 
@@ -27,6 +33,14 @@ module.exports = {
 
     error: {
       description: 'Unexpected error occurred'
+    },
+
+    forbidden: {
+      description: 'Provided secret is invalid or corrupted.  Please reauthenticate.'
+    },
+
+    requestFailed: {
+      description: 'Could not communicate with Treeline.io -- are you connected to the internet?'
     },
 
     success: {
@@ -44,7 +58,36 @@ module.exports = {
 
     var Http = require('machinepack-http');
 
-    return exits.success();
+
+    // Send an HTTP request and receive the response.
+    Http.sendHttpRequest({
+      method: 'get',
+      baseUrl: inputs.baseUrl||'http://api.treeline.io',
+      url: '/cli/apps',
+      params: {},
+      headers: {
+       'x-auth': inputs.secret
+      },
+    }).exec({
+      // An unexpected error occurred.
+      error: function(err) {
+        return exits.error(err);
+      },
+      // 403 status code returned from server
+      forbidden: function(result) {
+        return exits.forbidden(result.body);
+      },
+      // Unexpected connection error: could not send or receive HTTP request.
+      requestFailed: function() {
+        return exits.requestFailed();
+      },
+      // OK.
+      success: function(result) {
+        console.log(result.body);
+        return exits.success(result.body);
+      },
+    });
+
   }
 
 
