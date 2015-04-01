bec9c7963ffad7fa40f83bc43cf7f2de58d6ea40
Scott Gress <scottmgress@gmail.com>
2015-04-01 14:52:58 -0700

    Stub out treeline deploy

---
+++ bin/treeline-deploy.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+var chalk = require('chalk');
+console.log(chalk.red("Sorry, the `treeline deploy` command is not implemented during beta!"));
+console.log("Please contact support if you need help deploying your app.");
