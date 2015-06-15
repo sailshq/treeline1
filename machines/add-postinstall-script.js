25839cfb639c160fa6bffdc8c82513b6a20d4e2c
Cody Stoltman <particlebanana@gmail.com>
2015-06-15 17:55:34 -0500

    refactor adding postinstall script into a machine

---
+++ machines/add-postinstall-script.js
@@ -0,0 +1,90 @@
+module.exports = {
+
+
+  friendlyName: 'Add PostInstall Script',
+
+
+  description: 'Adds a postinstall script to a package.',
+
+
+  inputs: {
+
+    destination: {
+      description: 'Absolute path where the postinstall file will be copied.',
+      example: '/Users/mikermcneil/Desktop/foo'
+    }
+
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred'
+    },
+
+    success: {
+      description: 'Done.'
+    },
+
+  },
+
+
+  fn: function (inputs, exits){
+
+    var Filesystem = require('machinepack-fs');
+    var path = require('path');
+
+    Filesystem.exists({
+      path: path.resolve(inputs.destination, 'postinstall.js')
+    }).exec({
+
+      error: function(err) {
+        return exits.error(err);
+      },
+
+      success: function() {
+        return exits.success();
+      },
+
+      // Lets copy it over
+      doesNotExist: function() {
+
+        // Copy the postinstall script into the new project's files
+        Filesystem.read({
+          source: path.resolve(__dirname, '..', 'lib', 'apiPostInstallScript.js')
+        }).exec({
+          error: function (err) {
+            return exits.error(err);
+          },
+          doesNotEist: function () {
+            return exits.error();
+          },
+          success: function(js) {
+            var postInstallPath = path.resolve(inputs.destination, 'postinstall.js');
+
+            Filesystem.write({
+              destination: postInstallPath,
+              string: js
+            }).exec({
+              error: function (err) {
+                return exits.error(err);
+              },
+              alreadyExists: function (){
+                return exits.success();
+              },
+              success: function() {
+                return exits.success();
+              }
+            });
+          }
+        });
+
+      }
+    });
+  }
+
+};
