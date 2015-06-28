6b6f025a1e879d62f78209a7f7cf9733fce752f3
Mike McNeil <mike@balderdash.co>
2015-06-28 15:34:02 -0500

    Better console msg for 'treeline deploy'

--- bin/treeline-deploy.js
+++ bin/treeline-deploy.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 var chalk = require('chalk');
-console.log(chalk.red("Sorry, the `treeline deploy` command is not implemented during beta!"));
-console.log("Please contact support if you need help deploying your app.");
+console.log(chalk.red('Sorry, the `treeline deploy` command cannot be used during the beta.'));
+console.log('Fortunately, deploying a Treeline app is just like deploying any other Sails.js app (or Node.js app for that matter).');
+console.log('You can read more about deploying Sails.js/Treeline apps here:');
+console.log(chalk.blue(chalk.underline('http://sailsjs.org/documentation/concepts/deployment')));
+console.log();
+console.log('Please contact support@treeline.io if you need additional help getting your app running in production.');
