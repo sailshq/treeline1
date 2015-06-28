f918ed0c8ac8d12fdcfe587181b6ba137ee9a3c3
Mike McNeil <mike@balderdash.co>
2015-06-28 15:29:36 -0500

    Improve success console msg for people who don't know wtf sails.js is

--- bin/treeline-new.js
+++ bin/treeline-new.js
@@ -9,7 +9,8 @@ require('machine-as-script')({
 }).exec({
 
   success: function (){
-    console.log('New Treeline app generated.');
+    console.log('The files and folders for your new app have been generated!');
+    console.log('Now `cd` into the new app and run `treeline lift` to get started.');
   }
 
 });
